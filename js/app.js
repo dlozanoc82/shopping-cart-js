99219c82ab5f5ca909d124530593cd73ebfed4d2
Daniel <dlozanoc862@gmail.com>
2023-02-07 21:01:20 -0500

    Leer el html y extraer la informacion de los cursos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
 // Variables
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody')
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+
+cargarEventListeners();
+function cargarEventListeners(){
+    // Captura el evento "Agregar al Carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+//Funciones
+function agregarCurso(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+// Lee el HTML y extrae la información del curso
+function leerDatosCurso(curso){
+    console.log(curso);
+    // Crear objeto con el contenido del curso
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
+}
