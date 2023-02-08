de9f9f6cbe9bfcde34207f53a4f7546056ac491d
Daniel <dlozanoc862@gmail.com>
2023-02-07 20:22:51 -0500

    Archivos iniciales del proyecto carrito de compras

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody')
